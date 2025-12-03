fd8b5b191cef542a62a8ce559d05ae36b0fb8de0
Rita Kamenetskiy <rita.kamenetskiy@gmail.com>
2025-12-02 23:09:38 -0800

    Fixed the Pep8 errors
    Wanted to ensure there was no long lines or trailing spaces

--- tests/test_ritahw3.py
+++ tests/test_ritahw3.py
@@ -2,7 +2,7 @@
 Tests for the test_protein_fetch function
 """
 import pytest
-import numpy as np
+
 
 from ppinsight import protein_fetch
 
@@ -11,7 +11,8 @@ from ppinsight import protein_fetch
 accession_list_simple = ['P15692']
 accession_list_3 = ['P04637', 'P68871', 'Q8NEC1']
 # This is the function to call:
-# protein_fetch.get_uniprot_data(accession_list, fasta_file="proteins.fasta", csv_file="protein")
+# protein_fetch.get_uniprot_data(accession_list, fasta_file="proteins.fasta",
+# csv_file="protein")
 
 
 def test_smoke_rita():
@@ -20,9 +21,11 @@ def test_smoke_rita():
     reviewer: wmavila
     category: smoke test
 
-    Simple smoke test to ensure function runs 
+    Simple smoke test to ensure function runs
     """
-    protein_fetch.get_uniprot_data(accession_list_simple, fasta_file="proteins.fasta", csv_file="protein")
+    protein_fetch.get_uniprot_data(accession_list_simple,
+                                   fasta_file="proteins.fasta",
+                                   csv_file="protein")
     return
 
 
@@ -33,16 +36,24 @@ def test_get_uniprot_data_p15692_structured():
     category: one-shot test
 
     One-shot test with a known UniProt accession P15692 (VEGFA_HUMAN).
-    Expected structured data should match exactly! 
+    Expected structured data should match exactly!
     """
     expected_structured_data = [{
         'ID': 'sp|P15692|VEGFA_HUMAN',
         'Name': 'sp|P15692|VEGFA_HUMAN',
-        'Description': 'sp|P15692|VEGFA_HUMAN Vascular endothelial growth factor A, long form OS=Homo sapiens OX=9606 GN=VEGFA PE=1 SV=3',
+        'Description': 'sp|P15692|VEGFA_HUMAN Vascular endothelial growth '
+        'factor A, long form OS=Homo sapiens OX=9606 GN=VEGFA PE=1 SV=3',
         'Sequence Length': 395,
-        'Sequence': 'MTDRQTDTAPSPSYHLLPGRRRTVDAAASRGQGPEPAPGGGVEGVGARGVALKLFVQLLGCSRFGGAVVRAGEAEPSGAARSASSGREEPQPEEGEEEEEKEEERGPQWRLGARKPGSWTGEAAVCADSAPAARAPQALARASGRGGRVARRGAEESGPPHSPSRRGSASRAGPGRASETMNFLLSWVHWSLALLLYLHHAKWSQAAPMAEGGGQNHHEVVKFMDVYQRSYCHPIETLVDIFQEYPDEIEYIFKPSCVPLMRCGGCCNDEGLECVPTEESNITMQIMRIKPHQGQHIGEMSFLQHNKCECRPKKDRARQEKKSVRGKGKGQKRKRKKSRYKSWSVPCGPCSERRKHLFVQDPQTCKCSCKNTDSRCKARQLELNERTCRCDKPRR'
+        'Sequence': 'MTDRQTDTAPSPSYHLLPGRRRTVDAAASRGQGPEPAPGGGVEGVGARGV'
+        'ALKLFVQLLGCSRFGGAVVRAGEAEPSGAARSASSGREEPQPEEGEEEEEKEEERGPQWRLG'
+        'ARKPGSWTGEAAVCADSAPAARAPQALARASGRGGRVARRGAEESGPPHSPSRRGSASRAGPG'
+        'RASETMNFLLSVHWSLALLLYLHHAKWSQAAPMAEGGGQNHHEVVKFMDVYQRSYCHPIETLV'
+        'DIFQEYPDEIEYIFKPSCVPLMRCGGCCNDEGLECVPTEESNITMQIMRIKPHQGQHIGEMSFL'
+        'QHNKCECRPKKDRARQEKKSVRGKGKGQKRKRKKSRYKSWSVPCGPCSERRKHLFVQDPQTCKC'
+        'SCKNTDSRCKARQLELNERTCRCDKPRR'
     }]
-    actual_structured_data, actual_pdb_info = protein_fetch.get_uniprot_data(["P15692"])
+    actual_structured_data, actual_pdb_info = protein_fetch.get_uniprot_data(
+        ["P15692"])
 
     assert actual_structured_data == expected_structured_data
 
@@ -57,7 +68,8 @@ def test_get_uniprot_data_p15692_pdbinfo():
     Expected PDB info should match exactly!
     """
     expected_pdb_info = {'P15692': '1BJ1'}
-    actual_structured_data, actual_pdb_info = protein_fetch.get_uniprot_data(["P15692"])
+    actual_structured_data, actual_pdb_info = protein_fetch.get_uniprot_data(
+        ["P15692"])
 
     assert actual_pdb_info == expected_pdb_info
 
@@ -68,13 +80,13 @@ def test_nonexistent_protein():
     reviewer: wmavila
     category: edge test
 
-    When get_uniprot_data is called with a protein that doesn't exist or that 
+    When get_uniprot_data is called with a protein that doesn't exist or that
     can't be downloaded, I want get_uniprot_data to do:
     - Say that that protein ID was not correct within the Uniprot DataBase
     - I will want the function to stop and error out
         - raise some type of error saying that the value of the ID was bad
             - ValueError makes sense
-    - Even if there are other protein IDs that are correct, I want the 
+    - Even if there are other protein IDs that are correct, I want the
     function to stop
     """
     with pytest.raises(ValueError, match="Could not fetch FASTA for"):
@@ -94,9 +106,10 @@ def test_uniprot_fasta_parsing_pattern():
         structured_data, _ = protein_fetch.get_uniprot_data(accession_ids)
         assert len(structured_data) == len(accession_ids)
         for record in structured_data:
-            assert record["Sequence Length"] > 0  # Sequence shouldn't be empty!
+            assert record["Sequence Length"] > 0  # Not empty seq
         return
 
     # Run for different sizes of accession lists
-    for accession_ids in [["P15692"], ["P15692", "P69905"], ["P15692","P69905","P68871"]]:
+    for accession_ids in [["P15692"], ["P15692", "P69905"],
+                          ["P15692", "P69905", "P68871"]]:
         test(accession_ids)
