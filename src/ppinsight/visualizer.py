11aa1168357e8493e7250f70d00029b7e2bb39ef
Fiona McLary <fiona.emw@gmail.com>
2025-12-09 14:36:22 -0800

    More visualizer edits, streamlined tests, vis.py works now hooray!

--- src/ppinsight/visualizer.py
+++ src/ppinsight/visualizer.py
@@ -2,10 +2,10 @@
 Generates a pyplot bar chart comparing chosen models for a given score type
 """
 
-from matplotlib import pyplot as plt
 import os
+
+from matplotlib import pyplot as plt
 import pandas as pd
-from pathlib import Path
 
 def to_plot(models: list[os.PathLike]) -> list[pd.DataFrame]:
     """Create a list of dataframes for the models you want to plot"""
@@ -15,22 +15,20 @@ def to_plot(models: list[os.PathLike]) -> list[pd.DataFrame]:
         pass
     frames = []
     for m in models:
-        df = pd.read_csv(m)
+        df = pd.read_csv(m,header=0)
         frames.append(df)
     return frames
 
-def compare_scores(frames: list[pd.DataFrame], score_type: str, plot_title: str):
+def compare_scores(frames: list[pd.DataFrame], models: list[str], score_type: str, plot_title: str):
     """Generate bar chart showing the value of score_type for each model in frames"""
     scores = []
-    for file in frames:
-        if score_type not in file.columns:
-            raise LookupError(f'The score type {score_type} does not exist for the {file} model')
+    for df in frames:
+        if score_type not in df.columns:
+            raise LookupError(f'The score type {score_type} does not exist for the {df} model')
         else:
             pass
-        ind = file.columns.get_iloc(score_type)
-        cell = file.iloc[0,ind]
-        scores.append(cell)
-    plt.bar(frames, scores)
+        scores.append(df['score_a'][0])
+    plt.bar(models, scores)
     plt.title(plot_title)
     plt.xlabel('Interaction model')
     plt.ylabel(f'{score_type} value')
--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -3,13 +3,8 @@ Tests for the visualizer module
 """
 
 import pytest
-from matplotlib import pyplot as plt
-import numpy as np
-import os
-import pandas as pd
-from pathlib import Path
 
-from ppinsight import visualizer
+from ppinsight import visualizer as vis
 
 # setup variables
 models = ['../dummy_scores/model_1.csv','/home/fmclary/CSE583/PPInsight/dummy_scores/model_2.csv','/home/fmclary/CSE583/PPInsight/dummy_scores/model_3.csv']
@@ -19,7 +14,7 @@ def test_not_a_list():
     with pytest.raises(
         TypeError, match='Format your input as a list of file paths'
     ):
-        to_plot('../dummy_scores/model_1.csv','../dummy_scores/model_2.csv')
+        vis.to_plot('../dummy_scores/model_1.csv','../dummy_scores/model_2.csv')
     return
     
 def test_score_type_does_not_exist():
@@ -28,22 +23,22 @@ def test_score_type_does_not_exist():
         LookupError, match=f'The score type {score_type} does not exist for the {file} model'
     ):
         frames = pd.DataFrame(np.array([1,2,3]),columns=['a','b','c'])
-        compare_scores(frames, 'd', 'test plot title')
+        vis.compare_scores(frames, 'd', 'test plot title')
     return
 
 def test_smoke():
     """Basic smoke test to ensure function runs"""
-    to_plot(models)
+    vis.to_plot(models)
     return
 
 def test_output_is_df(dfs):
     """Pattern test to ensure output of to_plot is a list of pandas dataframes"""
     def setup(output):
-        output.dfs = to_plot(models)
+        output.dfs = vis.to_plot(models)
     output.assertIsInstance(dfs is pd.DataFrame)
     return
 
 def test_graph_dummy_models():
     """One-shot test to ensure compare_scores graphs score_a for the 3 dummy models as expected"""
-    compare_scores(to_plot(models), 'score_a', 'test plot title')
+    vis.ompare_scores(vis.to_plot(models), 'score_a', 'test plot title')
     return
