ed6de2e0fd968b4c909f675fbe47ec428566e6ac
Fiona McLary <fiona.emw@gmail.com>
2025-12-09 14:54:34 -0800

    Pylint has bestowed upon this file a 10/10, rejoice!

--- src/ppinsight/visualizer.py
+++ src/ppinsight/visualizer.py
@@ -9,10 +9,8 @@ import pandas as pd
 
 def to_plot(models: list[os.PathLike]) -> list[pd.DataFrame]:
     """Create a list of dataframes for the models you want to plot"""
-    if type(models) is not list:
+    if isinstance(models,list) is False:
         raise TypeError('Format your input as a list of file paths')
-    else:
-        pass
     frames = []
     for m in models:
         df = pd.read_csv(m,header=0)
@@ -25,8 +23,6 @@ def compare_scores(frames: list[pd.DataFrame], models: list[str], score_type: st
     for df in frames:
         if score_type not in df.columns:
             raise LookupError(f'The score type {score_type} does not exist for the {df} model')
-        else:
-            pass
         scores.append(df['score_a'][0])
     plt.bar(models, scores)
     plt.title(plot_title)
